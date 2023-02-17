b2596246f544a183734e70093d4cae01419aa0a9
Willatendo <williamjswindler@gmail.com>
2023-02-16 21:08:43 -0700

    lost worlds downloads

--- code.js
+++ code.js
@@ -29,19 +29,3 @@ function selectMod(evt, mod) {
 }
 
 document.getElementById("defaultMod").click();
-
-function download(file, text) {
-	var element = document.createElement('a');
-	element.setAttribute('href', 'data:text/plain;charset=utf-8, ' + encodeURIComponent(text));
-	element.setAttribute('download', file);
-	document.body.appendChild(element);
-	element.click();
-	document.body.removeChild(element);
-}
-
-document.getElementById("btn").addEventListener("click", function() {
-	var text = document.getElementById("text").value;
-	var filename = "lost_worlds/versions/1.16.5/lostworlds-alpha.11c.jar";
-	
-	download(filename, text);
-}, false);
