ce07b6eb26d72a7a29b4512ae1b907b03772bacf
Willatendo <williamjswindler@gmail.com>
2024-09-23 16:29:14 -0600

    3.14.0

---
+++ fossilsmodel.js
@@ -0,0 +1,8 @@
+BBPlugin.register('fossilsmodel', {
+	title: "Fossils Models",
+	author: "Willatendo",
+	description: "Makes models the Fossils and Archaeology: Legacy mod can read as .jsons",
+	version: "1.0.0",
+	icon: "fa-cube",
+	tags: ["Minecraft: Java Edition"]
+});
