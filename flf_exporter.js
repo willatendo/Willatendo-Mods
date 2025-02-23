b662ee14c18b293cef6031a2952d3d99f87ee8e8
Willatendo <williamjswindler@gmail.com>
2025-02-22 22:40:42 -0700

    Updates

--- flf_exporter.js
+++ flf_exporter.js
@@ -1,21 +1,22 @@
 (function () {
-    let exportModelButton;
-    let importModelButton;
-	let exportAnimationButton;
+    let exportJsonModelButton;
+	let exportJsonAnimationButton;
+    let exportJavaModelButton;
+	let exportJavaAnimationButton;
 	
 	Plugin.register("flf_exporter", {
 		title: "Fossil Legacy Format Exporter",
 		author: "Willatendo",
 		description: "Exports entity models into flf model format",
 		icon: "fa-cube",
-		version: "1.0.0",
+		version: "2.0.0",
 		variant: "both",
 		about: "This plugin exports your Blockbench entity models to be exported in flf model format and animations into fla.",
 		tags: ["Minecraft: Java Edition"],
 		onload() {
-			exportModelButton = new Action("export_flf", {
-				name: "Export to FLF",
-				description: "Exports entity models to FLF",
+			exportJsonModelButton = new Action("export_json_flf", {
+				name: "Export to Json FLF",
+				description: "Exports entity models to a json FLF model",
 				icon: "fa-file-export",
 				click() {
 					const fileName = Project.name.toLowerCase();
@@ -24,13 +25,13 @@
 						extensions: ["json"],
 						savetype: "text",
 						name: `${fileName}.json`,
-						content: autoStringify(generateModelFile())
+						content: autoStringify(generateJsonModelFile())
 					});
 				}
 			});
-			exportAnimationButton = new Action("export_fla", {
-				name: "Export to FLA",
-				description: "Exports animations to FLA format",
+			exportJsonAnimationButton = new Action("export_json_fla", {
+				name: "Export to Json FLA",
+				description: "Exports animations to a json FLA model",
 				icon: "fa-file-export",
 				click() {
 					const animation = Animation.selected;
@@ -40,42 +41,55 @@
 						extensions: ["json"],
 						savetype: "text",
 						name: `${animation.name.replaceAll(".", "_").replace("animation_", "").toLowerCase()}.json`,
-						content: autoStringify(generateAnimationFile(animation))
+						content: autoStringify(generateJsonAnimationFile(animation))
 					});
 				}
 			});
-			importModelButton = new Action("import_flf", {
-				name: "Import FLF",
-				description: "Imports FLF format json models",
-				icon: "fa-file-import",
+			exportJavaModelButton = new Action("export_java_flf", {
+				name: "Export to Java FLF",
+				description: "Exports entity models to a java FLF model for datagen purposes",
+				icon: "fa-file-export",
 				click() {
-					Blockbench.import({
-						type: "FLF Json",
-						extensions: ["json"],
-						readtype: "text"
-					}, (files) => {
-						loadModelFile(JSON.parse(files[0].content))
+					Blockbench.export({
+						type: "FLF Datagen",
+						extensions: ["txt"],
+						savetype: "text",
+						name: `${fileName}.txt`,
+						content: autoStringify(generateJavaAnimationFile())
+					});
+				}
+			});
+			exportJavaAnimationButton = new Action("export_java_fla", {
+				name: "Export to Java FLA",
+				description: "Exports animations to a java FLA model for datagen purposes",
+				icon: "fa-file-export",
+				condition: () => Format.animation_mode,
+				click() {
+					Blockbench.export({
+						type: "FLA Datagen",
+						extensions: ["txt"],
+						savetype: "text",
+						content: generateJavaAnimationFile()
 					});
 				}
 			});
 			
-			MenuBar.addAction(exportModelButton, "file.export");
-			MenuBar.addAction(exportAnimationButton, "file.export");
-			
-			MenuBar.addAction(importModelButton, "file.import")
+			MenuBar.addAction(exportJsonModelButton, "file.export");
+			MenuBar.addAction(exportJsonAnimationButton, "file.export");
+			MenuBar.addAction(exportJavaModelButton, "file.export");
+			MenuBar.addAction(exportJavaAnimationButton, "file.export");			
 		},
 		onunload() {
-			exportModelButton.delete();
-			exportAnimationButton.delete();
-			
-			importModelButton.delete();
+			exportJsonModelButton.delete();
+			exportJsonAnimationButton.delete();
+			exportJavaModelButton.delete();
+			exportJavaAnimationButton.delete();
 		}
 	});
 	
-	function generateModelFile() {
+	function generateJsonModelFile() {
 		const result = {
 			elements: [],
-			model_id: Project.name.toLowerCase(),
 			texture_height: Project.texture_height,
 			texture_width: Project.texture_width
 		};
@@ -150,11 +164,9 @@
 		return element;
 	}
 	
-	function generateAnimationFile(animation) {
+	function generateJsonAnimationFile(animation) {
 		const result = {
-			type: "json",
-			animations: [],
-			id: animation.name.toLowerCase(),
+			animation_channels: [],
 			length: animation.length
 		};
         if (animation.loop == "loop") {
@@ -164,13 +176,13 @@
             const boneAnimator = animation.animators[id];
             if (!(boneAnimator instanceof BoneAnimator)) continue;
             if (boneAnimator.position.length) {
-                result.animations.push(generateAnimation(boneAnimator.name, "position", boneAnimator.position));
+                result.animation_channels.push(generateAnimation(boneAnimator.name, "position", boneAnimator.position));
             }
             if (boneAnimator.rotation.length) {
-                result.animations.push(generateAnimation(boneAnimator.name, "rotation", boneAnimator.rotation));
+                result.animation_channels.push(generateAnimation(boneAnimator.name, "rotation", boneAnimator.rotation));
             }
             if (boneAnimator.scale.length) {
-                result.animations.push(generateAnimation(boneAnimator.name, "scale", boneAnimator.scale));
+                result.animation_channels.push(generateAnimation(boneAnimator.name, "scale", boneAnimator.scale));
             }
         }
 		return result;
@@ -185,7 +197,7 @@
 		
 		for (const keyframe of [...keyframes].sort((a, b) => a.time - b.time)) {
 			animation.keyframes.push({
-				degree_vec: {
+				vector: {
 					x: keyframe.get("x"),
 					y: keyframe.get("y"),
 					z: keyframe.get("z")
@@ -198,69 +210,83 @@
 		return animation;
 	}
 	
-	function loadModelFile(json) {
-		Project.name = json.model_id.replace(":", "_");
-		Project.texture_width = json.texture_width;
-		Project.texture_height = json.texture_height;
-		
-		var groups = {};
-		
-		json.elements.forEach(element => loadGroups(element));
-		
-		Canvas.updateAll();
-	}
 	
-	function loadGroups(element) {
-		var groupData = {
-			name: element.id
-		};
+	function generateJavaAnimationFile() {
+		let output = "";
 		
-		groupData.origin = [ element.poses.x, element.poses.y, element.poses.z ];
-		
-		var group = new Group(groupData);
-		
-		group.init();
+		for (const animation of Animation.all) {
+			output += `\npublic static final JsonAnimation ${animation.name.replaceAll(".", "_").replace("animation_", "").toUpperCase()} = JsonAnimation.builder(${animation.length}F)`;
+			
+			if (animation.loop === "loop") {
+				output += ".looping()";
+			}
 
-		element.boxes.forEach(box => loadCube(box, group));
-	
-		if(element.elements) {
-			element.elements.forEach(subElement => loadSubGroups(subElement, group));
+            for (const id in animation.animators) {
+				const boneAnimator = animation.animators[id];
+                if (!(boneAnimator instanceof BoneAnimator)) continue;
+				
+				let posKeyArray = [];
+                let rotKeyArray = [];
+                let scaleKeyArray = [];
+				
+				if (boneAnimator.position.length) {
+					output += `\n.addAnimation("${boneAnimator.name}", "position"`;
+					
+					for (const keyFrame of boneAnimator.position) {
+						posKeyArray.push(keyFrame);
+					}
+					
+					posKeyArray.sort((a, b) => a.time - b.time);
+					
+					for (const keyFrame of posKeyArray) {
+						const {x = 0.0, y = 0.0, z = 0.0} = keyFrame.data_points[0];
+						output += `,\n\t\tJsonKeyframe.create(${keyFrame.time}F, ${round2(x)}F, ${round2(y)}F, ${round2(z)}F, "${keyFrame.interpolation}")`;
+					}
+					
+					output += "))";
+				}
+				
+                if (boneAnimator.rotation.length) {
+					output += `\n.addAnimation("${boneAnimator.name}", "rotation"`;
+					
+					for (const keyFrame of boneAnimator.rotation) {
+						rotKeyArray.push(keyFrame);
+					}
+					
+					rotKeyArray.sort((a, b) => a.time - b.time);
+					
+					for (const keyFrame of rotKeyArray) {
+						const {x = 0.0, y = 0.0, z = 0.0} = keyFrame.data_points[0];
+						output += `,\n\t\tJsonKeyframe.create(${keyFrame.time}F, ${round2(x)}F, ${round2(y)}F, ${round2(z)}F, "${keyFrame.interpolation}")`;
+					}
+					
+					output += "))";
+				}
+				
+				if (boneAnimator.scale.length) {
+					output += `\n.addAnimation("${boneAnimator.name}", "scale"`;
+					
+					for (const keyFrame of boneAnimator.scale) {
+						scaleKeyArray.push(keyFrame);
+					}
+					
+					scaleKeyArray.sort((a, b) => a.time - b.time);
+					
+					for (const keyFrame of scaleKeyArray) {
+						const {x = 0.0, y = 0.0, z = 0.0} = keyFrame.data_points[0];
+						output += `,\n\t\tJsonKeyframe.create(${keyFrame.time}F, ${round2(x)}F, ${round2(y)}F, ${round2(z)}F, "${keyFrame.interpolation}")`;
+					}
+					
+					output += ")";
+				}
+			}
+            output += ".build();";
 		}
+		
+		return output.replaceAll("66666666666", "67").replaceAll("33333333333", "34");
 	}
 	
-	function loadSubGroups(element, parent) {
-		var groupData = {
-			name: element.id
-		};
-	
-		groupData.origin = [ element.poses.x, element.poses.y, element.poses.z ];
-	
-		var group = new Group(groupData);
-		
-		group.init();
-		
-		group.addTo(parent);
-
-		element.boxes.forEach(box => loadCube(box, group));
-		
-		if(element.elements) {
-			element.elements.forEach(subElement => loadSubGroups(subElement, group));
-		}
-	}
-	
-	function loadCube(box, parent) {
-		let xOrigin = parent.origin[0];
-		let zOrigin =  parent.origin[2];
-		let yOrigin = box.y_dimension + parent.origin[1];
-		var origin = [ xOrigin, zOrigin, yOrigin ];
-		
-		var cube = new Cube({
-			from: origin,
-			to: [ box.x_dimension, box.y_dimension, box.z_dimension ],
-			uv_offset: [ box.texture_x_offset, box.texture_y_offset ]
-		});
-		
-		cube.init();
-		cube.addTo(parent);
-	}
+    function round2(n) {
+        return Math.round((Number(n) + Number.EPSILON) * 100) / 100;
+    }
 })();
